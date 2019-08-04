87939d3e355217e87a2aa1a5adb59784db9315e8
Solvik Blum <solvik.blum@dailymotion.com>
2019-08-04 19:18:11 +0200

    check for get() function

--- netbox_agent/datacenter.py
+++ netbox_agent/datacenter.py
@@ -39,4 +39,6 @@ class Datacenter():
                 raise ImportError("Driver {} doesn't exists".format(self.driver))
 
     def get(self):
+        if not hasattr(self.driver, 'get'):
+            raise Exception("Your driver {} doesn't have a get() function, please fix it".format(self.driver))
         return getattr(self.driver, 'get')(self.driver_value, DATACENTER_LOCATION_REGEX)
