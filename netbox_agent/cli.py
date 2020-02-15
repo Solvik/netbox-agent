6a8d69af62367d7edfc8aa250c36dda9ca61f604
Solvik Blum <solvik@solvik.fr>
2020-02-15 20:05:12 +0100

    agent wasn't working anymore

--- netbox_agent/cli.py
+++ netbox_agent/cli.py
@@ -18,10 +18,11 @@ MANUFACTURERS = {
 
 
 def run(config):
-    manufacturer = dmidecode.get_by_type('Chassis')[0].get('Manufacturer')
+    dmi = dmidecode.parse()
+    manufacturer = dmidecode.get_by_type(dmi, 'Chassis')[0].get('Manufacturer')
 
     try:
-        server = MANUFACTURERS[manufacturer](dmi=dmidecode)
+        server = MANUFACTURERS[manufacturer](dmi=dmi)
     except KeyError:
         server = GenericHost
 
