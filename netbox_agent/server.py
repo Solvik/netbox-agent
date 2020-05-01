8a152cd15f8f9adbe444801f2b86fa0d8019d285
Solvik Blum <solvik@solvik.fr>
2020-05-01 19:36:25 +0200

    fix isort

--- netbox_agent/server.py
+++ netbox_agent/server.py
@@ -8,7 +8,7 @@ from netbox_agent.config import config
 from netbox_agent.config import netbox_instance as nb
 from netbox_agent.inventory import Inventory
 from netbox_agent.location import Datacenter, Rack, Tenant
-from netbox_agent.misc import create_netbox_tags, get_device_type, get_device_role
+from netbox_agent.misc import create_netbox_tags, get_device_role, get_device_type
 from netbox_agent.network import ServerNetwork
 from netbox_agent.power import PowerSupply
 
