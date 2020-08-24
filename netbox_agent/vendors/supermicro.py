e20e6a7deeb78108584c15830c0925776c2d3efb
Cyrinux <cyrinux@users.noreply.github.com>
2020-08-24 12:52:25 +0200

    Fix travis (#162)

--- netbox_agent/vendors/supermicro.py
+++ netbox_agent/vendors/supermicro.py
@@ -1,23 +1,21 @@
-
 from netbox_agent.location import Slot
 from netbox_agent.server import ServerBase
 
 
-"""
- Supermicro DMI can be messed up.  They depend on the vendor
- to set the correct values.  The endusers cannot
- change them without buying a license from Supermicro.
-
- There are 3 serial numbers in the system
-
-   1) System - this is used for the chassis information.
-   2) Baseboard - this is used for the blade.
-   3) Chassis - this is ignored.
-
-"""
-
-
 class SupermicroHost(ServerBase):
+    """
+     Supermicro DMI can be messed up.  They depend on the vendor
+     to set the correct values.  The endusers cannot
+     change them without buying a license from Supermicro.
+
+     There are 3 serial numbers in the system
+
+       1) System - this is used for the chassis information.
+       2) Baseboard - this is used for the blade.
+       3) Chassis - this is ignored.
+
+    """
+
     def __init__(self, *args, **kwargs):
         super(SupermicroHost, self).__init__(*args, **kwargs)
         self.manufacturer = 'Supermicro'
