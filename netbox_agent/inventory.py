b335b40079386633b960befe0ffe7f07659428de
CharlieRoot <wieger.bontekoe@gmail.com>
2024-03-26 13:53:29 +0100

    Change storage calculation

--- netbox_agent/inventory.py
+++ netbox_agent/inventory.py
@@ -341,11 +341,14 @@ class Inventory():
         for disk in self.lshw.get_hw_linux("storage"):
             if self.is_virtual_disk(disk, raid_devices):
                 continue
+
+            logging.info("Found disk size %s" % disk.get('size')
+
             if disk.get('type') != "NVME":
                 size = int(getattr(disk, "size", 0)) / 1073741824
             else:
                 size = int(getattr(disk, "size", 0))
-                
+
             d = {
                 "name": "",
                 'Size': '{} GB'.format(size),
