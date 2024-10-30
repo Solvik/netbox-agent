abaaea109c46fb3bc088754bdb1fbcefe8c56792
Tiago Teodósio <tiago.teodosio@brandwatch.com>
2024-10-30 15:58:14 +0100

    Bump version to 1.0.1-rc1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements():
 
 setup(
     name='netbox_agent',
-    version='1.0.0',
+    version='1.0.1-rc1',
     description='NetBox agent for server',
     long_description=open('README.md', encoding="utf-8").read(),
     long_description_content_type='text/markdown',
