dee1d39da18673aec0756f18504069ffc84be0c3
clbu <claudia.bura@numberly.com>
2024-11-13 15:35:54 +0100

    test

--- tests.sh
+++ tests.sh
@@ -22,12 +22,12 @@ EOF
 docker compose pull
 docker compose up -d
 
-while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' http://$(docker-compose port netbox 8080))" != "200" ]]
+while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' http://$(docker compose port netbox 8080))" != "200" ]]
 do
     sleep 5
 done
 
-export NETBOX_AGENT__NETBOX__URL="http://$(docker-compose port netbox 8080)"
+export NETBOX_AGENT__NETBOX__URL="http://$(docker compose port netbox 8080)"
 export NETBOX_AGENT__NETBOX__TOKEN='0123456789abcdef0123456789abcdef01234567'
 
 cd -
