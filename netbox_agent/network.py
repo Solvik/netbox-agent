3281ad6fae3c92913c6ae81698cb597704adc53a
Mathis Ribet <mathis.ribet@2021.icam.fr>
2025-01-20 22:32:15 +0100

    Enable/disable NICs depending on their link status

--- netbox_agent/network.py
+++ netbox_agent/network.py
@@ -300,6 +300,9 @@ class Network(object):
         if nic["mtu"]:
             params["mtu"] = nic["mtu"]
 
+        if nic["ethtool"] and nic["ethtool"].get("link") == "no":
+            params["enabled"] = False
+
         interface = self.nb_net.interfaces.create(**params)
 
         if nic["vlan"]:
