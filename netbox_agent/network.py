f26692d911f58a5f8a141e2a4c0f82666b8b92b2
Charles Luzzato <charles.luzzato@luzzato.com>
2025-06-16 23:45:41 +0200

    Need to specify the attached interface id to be able to get the correct MAC when multiple interfaces have the same max

--- netbox_agent/network.py
+++ netbox_agent/network.py
@@ -551,7 +551,7 @@ class Network(object):
                 if version.parse(nb.version) < version.parse("4.2"):
                     interface.mac_address = nic["mac"]
                 else:
-                    interface.primary_mac_address = {"mac_address": nic["mac"]}
+                    interface.primary_mac_address = {"mac_address": nic["mac"], "assigned_object_id": interface.id}
                 nic_update += 1
 
             if hasattr(interface, "mtu"):
