c84fab5e2e29919b5ce637f745e35727f068cd6b
Charles Luzzato <charles.luzzato@luzzato.com>
2025-06-21 15:02:27 +0200

    Changed ID

--- netbox_agent/network.py
+++ netbox_agent/network.py
@@ -663,7 +663,7 @@ class Network(object):
                 if version.parse(nb.version) < version.parse("4.2"):
                     interface.mac_address = nic["mac"]
                 else:
-                    nb_macs = list(self.nb_net.mac_addresses.filter(interface_id=nic.id))
+                    nb_macs = list(self.nb_net.mac_addresses.filter(interface_id=interface.id))
                     interface.primary_mac_address = {"mac_address": nic["mac"], "assigned_object_id": nb_macs[0].id}
                 nic_update += 1
 
